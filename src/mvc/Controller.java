e88ef1c405f271af4144cf9ab4f604d307b09b59
Michael Muré <mure.michael@gmail.com>
2011-04-06 11:08:37 +0200

    ajout des action de delete/advance/retreat

--- src/mvc/Controller.java
+++ src/mvc/Controller.java
@@ -16,148 +16,176 @@ import mvc.MainWindow;
 import mvc.View;
 
 public class Controller {
-	private View view;
-	private Point position;
-	private ArrayList<ImagePanel> imagePanels;
-	private Controller controller;
-	private ImageListModel listModel;
-	
-	public Controller(View view){
-		this.view = view;
-		this.position = new Point(0, 0);
-		this.controller = this;
-		
-		this.listModel = new ImageListModel();
-		view.getMainWindow().getImageList().setModel(this.listModel);
-		
-		initMainWindowListener();
-		initFCWindowListener();
-	}
+  private View view;
+  private Point position;
+  private ArrayList<ImagePanel> imagePanels;
+  private Controller controller;
+  private ImageListModel listModel;
 
-	/* MainWindow */
-	/**
-	 * Initialize the listener for the main window
-	 */
-	private void initMainWindowListener() {
-		MainWindow mw = this.view.getMainWindow();
-		mw.addItemQuitListener(new ItemQuitListener());
-		mw.addItemImportListener(new ItemImportListener());
-		mw.addItemDeleteListener(new ItemDeleteListener());
-		mw.addItemAdvanceListener(new ItemAdvanceListener());
-		mw.addItemRetreatListener(new ItemRetreatListener());
-		imagePanels = new ArrayList<ImagePanel>();
-	}
-	
-	/**
-	 * Action listener for the quit button of the main window
-	 */
-	class ItemQuitListener implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-			view.disposeAll();
-		}
-	}
-	
-	/**
-	 * Action listener for the import button in the main window
-	 */
-	class ItemImportListener implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-			view.showFileChooserWindow();
-		}
-	}
-	
-	/**
-	 * Action listener for the import button in the main window
-	 */
-	class ItemDeleteListener implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-		}
-	}
-	
-	/**
-	 * Action listener for the import button in the main window
-	 */
-	class ItemAdvanceListener implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-		}
-	}
-	
-	/**
-	 * Action listener for the import button in the main window
-	 */
-	class ItemRetreatListener implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-		}
-	}
-	
-	/* FileChooserWindow */
-	/**
-	 * Initialize the File chooser window listener
-	 */
-	private void initFCWindowListener() {
-		this.view.getFileChooserWindow().addFCActionListener(new FCActionListener());
-		this.view.getFileChooserWindow().addFCWindowListener(new FCWindowListener());
-	}
-	
-	/**
-	 * Action listener for the file chooser
-	 */
-	class FCActionListener implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-			if (JFileChooser.APPROVE_SELECTION.equals(e.getActionCommand())) {
-				String filename = view.getFileChooserWindow().getPath();
-				String description = view.getFileChooserWindow().getFilename();
-				
-				File file = new File(filename);
-				if(file.exists()) {
-					ImagePanel panel = new ImagePanel(filename, description, position);
-				    view.getMainWindow().getPanel().add(panel);
-				    imagePanels.add(panel);
-				    
-				    ImagePanelMouseListener ipml = new ImagePanelMouseListener(controller, panel);
-				    panel.addMouseListener(ipml);
-				    panel.addMouseMotionListener(ipml);
-				    listModel.addElement(panel);
-				    
-				    view.getMainWindow().repaint();
-				    position.setLocation(position.getX() + 10, position.getY() + 10);
+  public Controller(View view){
+    this.view = view;
+    this.position = new Point(0, 0);
+    this.controller = this;
 
-				}
-			} else if (JFileChooser.CANCEL_SELECTION.equals(e.getActionCommand())) {
-				// Annulation
-			}
-			// Fermeture de la fenêtre
-			view.hideFileChooserWindow();
-		}
-	}
+    this.listModel = new ImageListModel();
+    view.getMainWindow().getImageList().setModel(this.listModel);
 
-	/**
-	 * Window adaptater for the file chooser window
-	 */
-	class FCWindowListener extends WindowAdapter {
-		public void windowClosing(WindowEvent e) {
-			view.hideFileChooserWindow();
-		}
-	}
+    initMainWindowListener();
+    initFCWindowListener();
+  }
 
-	public void clickOn(ImagePanel panel, MouseEvent event) {
-		
-		
-		if(event.isShiftDown()) {
-			// Sélection multiple
-			if(panel.isSelected()) 	// le panel est déjà sélectionné => on désélectionne
-				panel.setSelected(false);
-			else					// le panel n'est pas sélectionné => on sélectionne
-				panel.setSelected(true);
-		} else {
-			// Sélection unique
+  /* MainWindow */
+  /**
+   * Initialize the listener for the main window
+   */
+  private void initMainWindowListener() {
+    MainWindow mw = this.view.getMainWindow();
+    mw.addItemQuitListener(new ItemQuitListener());
+    mw.addItemImportListener(new ItemImportListener());
+    mw.addItemDeleteListener(new ItemDeleteListener());
+    mw.addItemAdvanceListener(new ItemAdvanceListener());
+    mw.addItemRetreatListener(new ItemRetreatListener());
+    imagePanels = new ArrayList<ImagePanel>();
+  }
 
-			System.out.println("click dans Controller, sélection unique");
-			for (ImagePanel ip : this.imagePanels) {
-			    ip.setSelected(false);
-			}
-			panel.setSelected(true);
-		}
-		view.getMainWindow().repaint();
-	}
+  /**
+   * Action listener for the quit button of the main window
+   */
+  class ItemQuitListener implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+      view.disposeAll();
+    }
+  }
+
+  /**
+   * Action listener for the import button in the main window
+   */
+  class ItemImportListener implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+      view.showFileChooserWindow();
+    }
+  }
+
+   /**
+   * Action listener for the import button in the main window
+   */
+  class ItemDeleteListener implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+      ArrayList<ImagePanel> tmp = new ArrayList<ImagePanel>();
+      for (ImagePanel panel : imagePanels) {
+        if (panel.isSelected()) {
+          tmp.add(panel);
+          view.getMainWindow().getPanel().remove(panel);
+        }
+      }
+
+      /* On stocke dans une liste temporaire les image panel a supprimer
+       * pour éviter de modifier la liste en cours d'iteration */
+      for (ImagePanel panel : tmp) {
+        imagePanels.remove(panel);
+      }
+      view.getMainWindow().repaint();
+    }
+  }
+
+  /**
+   * Action listener for the import button in the main window
+   */
+  class ItemAdvanceListener implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+      for (ImagePanel panel : imagePanels) {
+        if (panel.isSelected()) {
+          int z = view.getMainWindow().getPanel().getComponentZOrder(panel);
+          view.getMainWindow().getPanel().setComponentZOrder(panel, z-1);
+        }
+      }
+      view.getMainWindow().repaint();
+    }
+  }
+
+  /**
+   * Action listener for the import button in the main window
+   */
+  class ItemRetreatListener implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+      for (ImagePanel panel : imagePanels) {
+        if (panel.isSelected()) {
+          int z = view.getMainWindow().getPanel().getComponentZOrder(panel);
+          view.getMainWindow().getPanel().setComponentZOrder(panel, z+1);
+        }
+      }
+      view.getMainWindow().repaint();
+    }
+  }
+
+  /* FileChooserWindow */
+  /**
+   * Initialize the File chooser window listener
+   */
+  private void initFCWindowListener() {
+    this.view.getFileChooserWindow().addFCActionListener(new FCActionListener());
+    this.view.getFileChooserWindow().addFCWindowListener(new FCWindowListener());
+  }
+
+  /**
+   * Action listener for the file chooser
+   */
+  class FCActionListener implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+      if (JFileChooser.APPROVE_SELECTION.equals(e.getActionCommand())) {
+        String filename = view.getFileChooserWindow().getPath();
+        String description = view.getFileChooserWindow().getFilename();
+
+        File file = new File(filename);
+        if(file.exists()) {
+          ImagePanel panel = new ImagePanel(filename, description, position);
+            view.getMainWindow().getPanel().add(panel);
+            imagePanels.add(panel);
+
+            ImagePanelMouseListener ipml = new ImagePanelMouseListener(controller, panel);
+            panel.addMouseListener(ipml);
+            panel.addMouseMotionListener(ipml);
+            listModel.addElement(panel);
+
+            view.getMainWindow().repaint();
+            position.setLocation(position.getX() + 10, position.getY() + 10);
+
+        }
+      } else if (JFileChooser.CANCEL_SELECTION.equals(e.getActionCommand())) {
+        // Annulation
+      }
+      // Fermeture de la fenêtre
+      view.hideFileChooserWindow();
+    }
+  }
+
+  /**
+   * Window adaptater for the file chooser window
+   */
+  class FCWindowListener extends WindowAdapter {
+    public void windowClosing(WindowEvent e) {
+      view.hideFileChooserWindow();
+    }
+  }
+
+  public void clickOn(ImagePanel panel, MouseEvent event) {
+
+
+    if(event.isShiftDown()) {
+      // Sélection multiple
+      if(panel.isSelected())  // le panel est déjà sélectionné => on désélectionne
+        panel.setSelected(false);
+      else          // le panel n'est pas sélectionné => on sélectionne
+        panel.setSelected(true);
+    } else {
+      // Sélection unique
+
+      System.out.println("click dans Controller, sélection unique");
+      for (ImagePanel ip : this.imagePanels) {
+          ip.setSelected(false);
+      }
+      panel.setSelected(true);
+    }
+    view.getMainWindow().repaint();
+  }
 }
